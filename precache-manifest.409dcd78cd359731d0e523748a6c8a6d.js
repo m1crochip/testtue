736ebcf734474dc3c6db80908f963fa7c9197a44
m1crochip <61010416@kmitl.ac.th>
2020-02-13 14:27:59 +0700

    Updates

---
+++ precache-manifest.409dcd78cd359731d0e523748a6c8a6d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d01dbf6840d39aac69f55719de22ca",
+    "url": "/testtue/index.html"
+  },
+  {
+    "revision": "443cb1cdd9534de919fb",
+    "url": "/testtue/static/css/2.8f16da89.chunk.css"
+  },
+  {
+    "revision": "fb6f118c45936e5dea8b",
+    "url": "/testtue/static/css/main.9406a71e.chunk.css"
+  },
+  {
+    "revision": "443cb1cdd9534de919fb",
+    "url": "/testtue/static/js/2.f5777666.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/testtue/static/js/2.f5777666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6f118c45936e5dea8b",
+    "url": "/testtue/static/js/main.de6dd88b.chunk.js"
+  },
+  {
+    "revision": "899e9e6642cb53244ad8",
+    "url": "/testtue/static/js/runtime-main.1fd1a79a.js"
+  },
+  {
+    "revision": "abc37022736bcc61af3127680be72032",
+    "url": "/testtue/static/media/download.abc37022.jpg"
+  },
+  {
+    "revision": "ff558db71fd9e245302f42459daee004",
+    "url": "/testtue/static/media/download1.ff558db7.jpg"
+  },
+  {
+    "revision": "c7343da5c9f7a39936ffab057efa2316",
+    "url": "/testtue/static/media/download3.c7343da5.jpg"
+  },
+  {
+    "revision": "92ca92bb33b8f319691c04e9a07f167d",
+    "url": "/testtue/static/media/profile.92ca92bb.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/testtue/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/testtue/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/testtue/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/testtue/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1c9dc1a1856b6b26e113e74596343aea",
+    "url": "/testtue/static/media/weblogo_white.1c9dc1a1.png"
+  }
+]);
